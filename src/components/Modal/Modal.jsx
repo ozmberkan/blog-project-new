f622f02bd01760f7b16b803211e7ad471d3ab4d4
ozmberkan <ozmberkan@gmail.com>
2024-07-20 22:17:04 +0300

    modal added

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,83 @@
+import { createPortal } from "react-dom";
+import PropTypes from "prop-types";
+import { GrFormClose } from "react-icons/gr";
+
+function Modal({ setIsShowModal }) {
+  function handleClose() {
+    setIsShowModal(false);
+  }
+
+  return createPortal(
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+      <div className="bg-zinc-950 border-2 border-zinc-800 dark:border-zinc-400 dark:bg-[#fff]  rounded-2xl shadow-lg p-6 max-w-xl w-full relative z-50">
+        <div className="flex w-full gap-y-2 flex-col mb-5">
+          <div className="flex justify-between items-center w-full">
+            <h3 className="text-xl font-semibold text-zinc-400 dark:text-zinc-700">
+              Blog Oluştur
+            </h3>
+            <button
+              className="text-gray-500 hover:text-zinc-300 dark:hover:text-zinc-800 hover:bg-zinc-800 p-1 flex items-center justify-center dark:hover:bg-[#f1f1f1] rounded-md transition-colors duration-500"
+              onClick={handleClose}
+            >
+              <GrFormClose size={25} />
+            </button>
+          </div>
+          <div className="text-sm text-zinc-500">
+            Bu alandan yeni bir blog ekleyebilirsin. Doldurman gereken alanlar
+            '*' ile belirtildi.
+          </div>
+        </div>
+        <div className="py-4 grid grid-cols-2 gap-3">
+          <div className="flex flex-col gap-y-2 font-lato">
+            <label className="font-semibold">Text</label>
+            <input
+              type="text"
+              className="p-3 w-full rounded-md border bg-transparent dark:border-zinc-600 border-zinc-700  placeholder:text-zinc-500"
+              placeholder="Başlık..."
+            />
+          </div>
+          <div className="flex flex-col gap-y-2 font-lato">
+            <label className="font-semibold">Text</label>
+            <input
+              type="text"
+              className="p-3 w-full rounded-md border bg-transparent dark:border-zinc-600 border-zinc-700  placeholder:text-zinc-500"
+              placeholder="Başlık..."
+            />
+          </div>
+          <div className="col-span-2 row-span-3 gap-y-2 flex flex-col font-lato">
+            <label className="font-semibold">Text</label>
+            <textarea
+              type="text"
+              className="p-3 w-full min-w-[200px] min-h-[200px] bg-transparent max-h-[200px] dark:border-zinc-600 border-zinc-700 rounded-md border  placeholder:text-zinc-500"
+              placeholder="Başlık..."
+            />
+          </div>
+          <div className="flex flex-col gap-y-2 font-lato">
+            <label className="font-semibold">Text</label>
+            <input
+              type="text"
+              className="p-3 w-full rounded-md border bg-transparent dark:border-zinc-600 border-zinc-700  placeholder:text-zinc-500"
+              placeholder="Başlık..."
+            />
+          </div>
+          <button className="p-3 w-full rounded-md bg-zinc-700  dark:bg-zinc-100 border">
+            Button
+          </button>
+        </div>
+      </div>
+      <div
+        className="bg-black bg-opacity-50 absolute w-full h-full"
+        onClick={handleClose}
+      ></div>
+    </div>,
+    document.getElementById("modal")
+  );
+}
+
+Modal.propTypes = {
+  setIsShowModal: PropTypes.func,
+  title: PropTypes.string,
+  desc: PropTypes.string,
+};
+
+export default Modal;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { HiMoon, HiSearch, HiSun, HiViewGridAdd } from "react-icons/hi";
 import { BsCardList } from "react-icons/bs";
+import Modal from "../Modal/Modal";
 
 const Navbar = () => {
   const [isLightMode, setIsLightMode] = useState(
@@ -8,6 +9,8 @@ const Navbar = () => {
       (!("theme" in localStorage) &&
         window.matchMedia("(prefers-color-scheme: dark)").matches)
   );
+
+  const [isShowModal, setIsShowModal] = useState(false);
 
   useEffect(() => {
     if (isLightMode) {
@@ -24,40 +27,46 @@ const Navbar = () => {
   };
 
   return (
-    <div className="py-4 px-80 bg-[#0F0F10] transition-all duration-500 dark:bg-[#fff] dark:border-zinc-300 flex items-center justify-between border-b border-zinc-700 w-full ">
-      <div>
-        <div className="font-lato cursor-pointer text-3xl font-semibold text-[#d4d2d2] dark:text-[#141414] flex items-center justify-center gap-x-2">
-          <BsCardList size={30} />
-          Blog Project
-        </div>
-      </div>
-      <div className="flex  items-center gap-x-2.5 ">
-        <div className="relative">
-          <label className="text-zinc-600 dark:text-black absolute right-5 top-3.5">
-            <HiSearch size={24} />
-          </label>
-          <input
-            type="text"
-            placeholder="Blog Ara..."
-            className="border bg-black  dark:bg-transparent dark:focus:ring-offset-white dark:focus:ring-2 py-3 px-5 placeholder:text-zinc-600 text-[#d4d2d2] font-lato rounded-full min-w-[300px] border-zinc-700 outline-none transition-all pr-12 duration-500  focus:ring-2 ring-zinc-800 ring-offset-[#0f0f10] ring-offset-2 "
-          />
-        </div>
-        <div
-          onClick={handleLightMode}
-          className=" p-3 cursor-pointer text-[#d4d2d2] dark:text-[#141414] dark:hover:bg-[#f1f1f1] hover:bg-zinc-800 rounded-md transition-colors duration-500 flex"
-        >
-          <div className="flex items-center justify-center gap-x-2">
-            {isLightMode ? <HiMoon size={26} /> : <HiSun size={26} />}
+    <Fragment>
+      <div className="py-4 px-80 bg-[#0F0F10] transition-all duration-500 dark:bg-[#fff] dark:border-zinc-300 flex items-center justify-between border-b border-zinc-700 w-full ">
+        <div>
+          <div className="font-lato cursor-pointer text-3xl font-semibold text-[#d4d2d2] dark:text-[#141414] flex items-center justify-center gap-x-2">
+            <BsCardList size={30} />
+            Blog Project
           </div>
         </div>
-        <div className="py-3 hover:bg-zinc-800 px-5 cursor-pointer text-[#d4d2d2] dark:text-[#141414] dark:bg-[#f1f1f1]  group transition-all duration-500 bg-zinc-900 rounded-md  flex">
-          <div className="flex items-center justify-center gap-x-2">
-            <HiViewGridAdd size={26} />
-            <span className="font-lato font-semibold ">Ekle</span>
+        <div className="flex  items-center gap-x-2.5 ">
+          <div className="relative">
+            <label className="text-zinc-600 dark:text-black absolute right-5 top-3.5">
+              <HiSearch size={24} />
+            </label>
+            <input
+              type="text"
+              placeholder="Blog Ara..."
+              className="border bg-black  dark:bg-transparent dark:focus:ring-offset-white dark:focus:ring-2 py-3 px-5 placeholder:text-zinc-600 text-[#d4d2d2] font-lato rounded-full min-w-[300px] border-zinc-700 outline-none transition-all pr-12 duration-500  focus:ring-2 ring-zinc-800 ring-offset-[#0f0f10] ring-offset-2 "
+            />
+          </div>
+          <div
+            onClick={handleLightMode}
+            className=" p-3 cursor-pointer text-[#d4d2d2] dark:text-[#141414] dark:hover:bg-[#f1f1f1] hover:bg-zinc-800 rounded-md transition-colors duration-500 flex"
+          >
+            <div className="flex items-center justify-center gap-x-2">
+              {isLightMode ? <HiMoon size={26} /> : <HiSun size={26} />}
+            </div>
+          </div>
+          <div
+            onClick={() => setIsShowModal(true)}
+            className="py-3 hover:bg-zinc-800 px-5 cursor-pointer text-[#d4d2d2] dark:text-[#141414] dark:bg-[#f1f1f1]  group transition-all duration-500 bg-zinc-900 rounded-md  flex"
+          >
+            <div className="flex items-center justify-center gap-x-2">
+              <HiViewGridAdd size={26} />
+              <span className="font-lato font-semibold ">Ekle</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      {isShowModal && <Modal setIsShowModal={setIsShowModal} />}
+    </Fragment>
   );
 };
 
