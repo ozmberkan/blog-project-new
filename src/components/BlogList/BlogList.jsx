ba6d12c938a041a61aeab87a8a32f43735a34014
Berkan Özmen <=>
2024-07-22 15:49:31 +0300

    project is done

--- src/components/BlogList/BlogList.jsx
+++ src/components/BlogList/BlogList.jsx
@@ -2,17 +2,37 @@ import React from "react";
 import BlogItem from "../BlogItem/BlogItem";
 import { useAutoAnimate } from "@formkit/auto-animate/react";
 
-const BlogList = ({ filteredData, setData, data }) => {
+const BlogList = ({ data, sortBy, selectedCategory, search, setData }) => {
   const [animationParent] = useAutoAnimate();
 
+  // Filter and sort data based on selectedCategory, search, and sortBy
+  const filteredAndSortedData = data
+    .filter((item) => {
+      const matchesCategory =
+        selectedCategory === "All" || item.category === selectedCategory;
+      const matchesSearch = `${item.title} ${item.authorName} ${item.date}`
+        .toLowerCase()
+        .includes(search.toLowerCase());
+
+      return matchesCategory && matchesSearch;
+    })
+    .sort((a, b) => {
+      if (sortBy === "title") {
+        return a.title.localeCompare(b.title);
+      } else if (sortBy === "date") {
+        return new Date(a.date) - new Date(b.date);
+      }
+      return 0;
+    });
+
   return (
-    <div className="w-full  flex flex-col   gap-y-12" ref={animationParent}>
-      {filteredData.length > 0 ? (
-        filteredData.map((blog) => (
+    <div className="w-full flex flex-col gap-y-12" ref={animationParent}>
+      {filteredAndSortedData.length > 0 ? (
+        filteredAndSortedData.map((blog) => (
           <BlogItem setData={setData} data={data} key={blog.id} blog={blog} />
         ))
       ) : (
-        <span className="bg-red-500 py-2 px-3 rounded-md text-red-200 dark:text-red-500 dark:bg-red-200 font-lato text-sm ">
+        <span className="bg-red-500 py-2 px-3 rounded-md text-red-200 dark:text-red-500 dark:bg-red-200 font-lato text-sm">
           Veritabanında herhangi bir blog bulunmamaktadır!
         </span>
       )}
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Filter from "../Filter/Filter";
 import BlogList from "../BlogList/BlogList";
 import { categories } from "~/data/blogData";
@@ -12,30 +12,6 @@ const Main = ({
 }) => {
   const [sortBy, setSortBy] = useState("title");
 
-  // Filter and sort data based on selectedCategory, search, and sortBy
-  useEffect(() => {
-    const filteredAndSortedData = data
-      .filter((item) => {
-        const matchesCategory =
-          selectedCategory === "All" || item.category === selectedCategory;
-        const matchesSearch = `${item.title} ${item.authorName} ${item.date}`
-          .toLowerCase()
-          .includes(search.toLowerCase());
-
-        return matchesCategory && matchesSearch;
-      })
-      .sort((a, b) => {
-        if (sortBy === "title") {
-          return a.title.localeCompare(b.title);
-        } else if (sortBy === "date") {
-          return new Date(a.date) - new Date(b.date);
-        }
-        return 0;
-      });
-
-    setData(filteredAndSortedData);
-  }, [selectedCategory, search, sortBy]);
-
   return (
     <div className="w-full h-svh bg-[#0F0F10] dark:bg-[#fdfdfd] transition-all duration-500 py-5 px-80">
       <Filter
@@ -45,7 +21,13 @@ const Main = ({
         setSortBy={setSortBy}
         sortBy={sortBy}
       />
-      <BlogList filteredData={data} setData={setData} data={data} />
+      <BlogList
+        data={data}
+        sortBy={sortBy}
+        selectedCategory={selectedCategory}
+        search={search}
+        setData={setData}
+      />
     </div>
   );
 };
